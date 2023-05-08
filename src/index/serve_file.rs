b1432f9ee54f92c9fc0bd7a1927395d88de8079d
snorkysnark <snorkysnark@gmail.com>
2023-05-08 12:21:15 +0200

    guess mime type when serving files

--- src/index/serve_file.rs
+++ src/index/serve_file.rs
@@ -1,7 +1,9 @@
 use axum::{
     body::StreamBody,
     extract::{Path, State},
+    headers::ContentType,
     response::IntoResponse,
+    TypedHeader,
 };
 use entity::types::NodeType;
 use relative_path::RelativePathBuf;
@@ -25,9 +27,11 @@ pub async fn serve_file_handler(
     let base_path = state.sources.get_base_path(node_type)?;
     let full_path = rel_path.to_path(base_path);
 
+    let mime = mime_guess::from_path(&full_path).first_or_text_plain();
+
     let file = tokio::fs::File::open(&full_path).await?;
     let stream = ReaderStream::new(file);
     let body = StreamBody::new(stream);
 
-    Ok(body)
+    Ok((TypedHeader(ContentType::from(mime)), body))
 }
