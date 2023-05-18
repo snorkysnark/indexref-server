baafb7bfa2762bdb83c516bff3e87ca3ab830fa8
snorkysnark <snorkysnark@gmail.com>
2023-05-19 00:37:08 +0200

    use json-viewer react library
    in a hacky way inside solidjs

--- client/src/JsonView.tsx
+++ client/src/JsonView.tsx
@@ -1,25 +1,55 @@
-import jsonview from "@pgrabovets/json-view";
 import { createEffect, createMemo, createSignal, onCleanup } from "solid-js";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { defineDataType, JsonViewer, stringType } from "@textea/json-viewer";
 
-export default function JsonView(props: { value: object | string }) {
-    const tree = createMemo(() => {
-        const treeObj = jsonview.create(props.value);
-        onCleanup(() => {
-            jsonview.destroy(treeObj);
-        });
+// Hack to use the JsonViewer react component in solid
 
-        return treeObj;
-    });
+const linkType = defineDataType({
+    ...stringType,
+    is(value) {
+        if (typeof value !== "string") return false;
+        try {
+            new URL(value);
+        } catch {
+            return false;
+        }
+        return true;
+    },
+    Component: (props) => {
+        const url = props.value.toString();
+        return React.createElement(
+            "a",
+            { href: url, target: "_blank", class: "link" },
+            url
+        );
+    },
+});
 
+export default function JsonView(props: { value: object }) {
     const [container, setContainer] = createSignal<HTMLElement>();
-    createEffect(() => {
-        if (tree() && container()) {
-            jsonview.render(tree(), container());
-            jsonview.expand(tree());
+    const root = createMemo(() => {
+        if (container()) {
+            const myRoot = createRoot(container());
+            onCleanup(() => myRoot.unmount());
+
+            return myRoot;
         }
     });
 
-    return (
-        <div class="w-full h-full" ref={setContainer}></div>
-    )
+    createEffect(() => {
+        if (root()) {
+            root().render(
+                React.createElement(JsonViewer, {
+                    value: props.value,
+                    valueTypes: [linkType],
+                    displayDataTypes: false,
+                    rootName: false,
+                    displaySize: false,
+                })
+            );
+        }
+    });
+
+    return <div class="w-full h-full text-lg" ref={setContainer}></div>;
 }
