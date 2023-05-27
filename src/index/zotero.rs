09571536ff2a3e56bf48b8d4aed966688a756689
snorkysnark <snorkysnark@gmail.com>
2023-05-27 14:04:50 +0200

    extract note title from html

--- src/index/zotero.rs
+++ src/index/zotero.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
 
+use chrono::{DateTime, NaiveDateTime};
 use eyre::ContextCompat;
 use hyper::HeaderMap;
+use once_cell::sync::Lazy;
 use reqwest::Client;
+use scraper::{Html, Selector};
 use sea_orm::{ActiveModelTrait, DatabaseConnection, Set};
 use serde::Deserialize;
 use serde_json::Value as JsonValue;
@@ -14,6 +17,7 @@ use crate::{
         types::{AttachedTableType, NodeType},
         zotero,
     },
+    ext::JsonValueExt,
 };
 
 #[derive(Debug, Deserialize)]
@@ -62,6 +66,32 @@ impl ZoteroSource {
     }
 }
 
+fn extract_title(item: &ZoteroItem) -> eyre::Result<Option<String>> {
+    static SELECT_H1: Lazy<Selector> = Lazy::new(|| Selector::parse("h1").unwrap());
+
+    Ok(match item.data.get_ok("itemType")?.as_str_ok()? {
+        "note" => {
+            let html = item.data.get_ok("note")?.as_str_ok()?;
+
+            Html::parse_fragment(html)
+                .select(&*SELECT_H1)
+                .next()
+                .map(|node| node.text().collect::<Vec<_>>().join(" "))
+        }
+        "annotation" => item
+            .data
+            .get("annotationText")
+            .or(item.data.get("annotationComment"))
+            .and_then(|v| v.as_str())
+            .map(ToOwned::to_owned),
+        _ => item
+            .data
+            .get("title")
+            .and_then(|v| v.as_str())
+            .map(ToOwned::to_owned),
+    })
+}
+
 pub async fn insert_from_source(
     db: &DatabaseConnection,
     source: &ZoteroSource,
@@ -78,16 +108,19 @@ pub async fn insert_from_source(
                 .map(|str| str.to_owned())
         }
 
-        let title = item
+        let title = extract_title(&item)?;
+        let date_modified = item
             .data
-            .get("title")
-            .or(item.data.get("annotationText"))
-            .map(string_value)
-            .transpose()?;
+            .get("dateModified")
+            .and_then(|v| v.as_str())
+            .map(|v| DateTime::parse_from_rfc3339(v))
+            .transpose()?
+            .map(|date| date.naive_utc());
         let parent_key = item.data.get("parentItem").map(string_value).transpose()?;
 
         let node_inserted = node::ActiveModel {
             r#type: Set(NodeType::Zotero),
+            created: Set(date_modified),
             attached_table: Set(Some(AttachedTableType::Zotero)),
             title: Set(title),
             original_id: Set(Some(item.key.clone())),
