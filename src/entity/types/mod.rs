103675a1ae07564d1345364c5a0f10134e4a672f
snorkysnark <snorkysnark@gmail.com>
2023-06-20 12:33:10 +0200

    scrapbook data field

--- src/entity/types/mod.rs
+++ src/entity/types/mod.rs
@@ -2,6 +2,6 @@ mod node_data;
 mod node_type;
 mod relative_path;
 
-pub use self::node_data::{NodeData, TelegramData, TextEntity};
+pub use self::node_data::*;
 pub use self::node_type::{AttachedTableType, NodeType, SourceFolderType};
 pub use self::relative_path::RelativePathSql;
