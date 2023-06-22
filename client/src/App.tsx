a985eeb758ff6a7e96797e4d2ac4adb0afebf036
snorkysnark <snorkysnark@gmail.com>
2023-06-22 19:48:33 +0200

    search box design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { createSignal, onCleanup, Show } from "solid-js";
 import NodeData from "./NodeData";
 import NodeTree from "./NodeTree";
+import { ImSearch } from 'solid-icons/im'
 import { getSet } from "./signals/getset";
 import { createNodes } from "./signals/server";
 import { selectNextNode, selectPrevNode } from "./treeNavigation";
@@ -28,10 +29,16 @@ export default function App() {
 
     return (
         <div class="flex h-screen">
-            <div class="w-1/2 overflow-y-scroll pb-4">
-                <Show when={nodes()}>
-                    <NodeTree nodes={nodes()} selectedId={selectedId} />
-                </Show>
+            <div class="w-1/2 overflow-hidden">
+                <div class="h-10 w-full p-1 flex items-center shadow-md">
+                    <ImSearch class="mx-1" />
+                    <input class="bg-transparent border-2 border-gray-200 shadow-inner w-full h-full" />
+                </div>
+                <div class="overflow-y-scroll pb-4 h-full">
+                    <Show when={nodes()}>
+                        <NodeTree nodes={nodes()} selectedId={selectedId} />
+                    </Show>
+                </div>
             </div>
             <div class="w-1/2 overflow-y-scroll">
                 <Show when={selectedId.get()}>
