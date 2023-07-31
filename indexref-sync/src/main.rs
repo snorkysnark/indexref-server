96f445c886414b2052d5af87d4eff3bcdeb5aacb
snorkysnark <snorkysnark@gmail.com>
2023-07-31 17:13:48 +0200

    filter what crates emit logs

--- indexref-sync/src/main.rs
+++ indexref-sync/src/main.rs
@@ -7,19 +7,14 @@ use std::path::PathBuf;
 
 use config::SourcesConfig;
 use migration::{Migrator, MigratorTrait};
-use sea_orm::{ConnectOptions, Database, DatabaseConnection, DbErr};
-
-async fn connect_db(url: String) -> Result<DatabaseConnection, DbErr> {
-    let mut opt = ConnectOptions::new(url);
-    opt.sqlx_logging_level(log::LevelFilter::Trace); // Too verbose
-
-    Database::connect(opt).await
-}
+use sea_orm::Database;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
+        // Filter what crates emit logs
+        .with_env_filter(EnvFilter::try_new("indexref_sync,sea_orm")?)
         .init();
 
     dotenv::dotenv().ok();
@@ -29,7 +24,7 @@ async fn main() -> eyre::Result<()> {
         .into();
     let sources: SourcesConfig = serde_json::from_str(&std::fs::read_to_string(&config_path)?)?;
 
-    let db = connect_db(db_url).await?;
+    let db = Database::connect(&db_url).await?;
     Migrator::up(&db, None).await?;
 
     file_sync::sync_db_with_sources(&db, &sources).await?;
