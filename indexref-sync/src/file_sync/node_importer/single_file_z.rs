4b300d35c82ba8d811cc9d22df9c72f6d3a79d55
snorkysnark <snorkysnark@gmail.com>
2023-08-08 22:04:14 +0200

    single_file_z: make future Sync

--- indexref-sync/src/file_sync/node_importer/single_file_z.rs
+++ indexref-sync/src/file_sync/node_importer/single_file_z.rs
@@ -20,45 +20,49 @@ pub async fn import_from_file(
     file_path: &Path,
     file_id: i32,
 ) -> Result<Vec<node::Model>> {
-    // The 'zip' portion of the file is NOT valid UTF-8,
-    // so we have to use lossy conversion
-    let bytes = fs::read(file_path)?;
-    let html = String::from_utf8_lossy(&bytes);
-    let document = Html::parse_document(&html);
+    let inserted = {
+        // The 'zip' portion of the file is NOT valid UTF-8,
+        // so we have to use lossy conversion
+        let bytes = fs::read(file_path)?;
+        let html = String::from_utf8_lossy(&bytes);
+        let document = Html::parse_document(&html);
 
-    let filename = file_path
-        .file_name()
-        .context("Not a file path")?
-        .to_str()
-        .context("Non-Utf8 filename")?;
+        let filename = file_path
+            .file_name()
+            .context("Not a file path")?
+            .to_str()
+            .context("Non-Utf8 filename")?;
 
-    let title = document
-        .select(&SEL_TITLE)
-        .next()
-        .and_then(|el| el.text().next())
-        .map(|str| str.to_owned());
-    let url = document
-        .select(&SEL_CANONICAL_LINK)
-        .next()
-        .and_then(|el| el.value().attr("href"))
-        .map(|str| str.to_owned());
-    let created = DATE_REGEX
-        .captures_iter(filename)
-        .next()
-        .and_then(|captures| captures.get(1))
-        .map(|date| {
-            NaiveDateTime::parse_from_str(date.as_str(), "%d.%m.%Y %H_%M_%S")
-                .with_context(|| date.as_str().to_owned())
-        })
-        .transpose()?;
+        let title = document
+            .select(&SEL_TITLE)
+            .next()
+            .and_then(|el| el.text().next())
+            .map(|str| str.to_owned());
+        let url = document
+            .select(&SEL_CANONICAL_LINK)
+            .next()
+            .and_then(|el| el.value().attr("href"))
+            .map(|str| str.to_owned());
+        let created = DATE_REGEX
+            .captures_iter(filename)
+            .next()
+            .and_then(|captures| captures.get(1))
+            .map(|date| {
+                NaiveDateTime::parse_from_str(date.as_str(), "%d.%m.%Y %H_%M_%S")
+                    .with_context(|| date.as_str().to_owned())
+            })
+            .transpose()?;
 
-    let inserted = node::ActiveModel {
-        file_id: Set(Some(file_id)),
-        node_type: Set(NodeType::SingleFileZ),
-        title: Set(title),
-        url: Set(url),
-        created: Set(created),
-        ..Default::default()
+        std::mem::drop(document);
+
+        node::ActiveModel {
+            file_id: Set(Some(file_id)),
+            node_type: Set(NodeType::SingleFileZ),
+            title: Set(title),
+            url: Set(url),
+            created: Set(created),
+            ..Default::default()
+        }
     }
     .insert(db)
     .await?;
